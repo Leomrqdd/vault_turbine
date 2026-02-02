5709994b4619282d110e0aae7edc292ef2a3fa80
Léo Marquand <leo-marquand@hotmail.fr>
2026-02-02 12:02:01 +0100

    add tests for init + deposit

--- tests/anchor-vault.ts
+++ tests/anchor-vault.ts
@@ -4,13 +4,55 @@ import { AnchorVault } from "../target/types/anchor_vault";
 
 describe("anchor-vault", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.anchorVault as Program<AnchorVault>;
 
-  it("Is initialized!", async () => {
+  const signer = new anchor.web3.Keypair()
+
+  const [vaultState, stateBump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vault_state"),signer.publicKey.toBuffer()],
+    program.programId
+  )
+  
+  const [vault, vaultBump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"),vaultState.toBuffer()],
+    program.programId
+  )
+
+
+
+before(async() => {
+  console.log("Airdropping to signer", signer.publicKey.toBase58());
+  await provider.connection.requestAirdrop(signer.publicKey,100*anchor.web3.LAMPORTS_PER_SOL)
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  console.log("Airdropped to signer", signer.publicKey.toBase58());
+  console.log("User balance", (await provider.connection.getBalance(signer.publicKey))/anchor.web3.LAMPORTS_PER_SOL);
+})
+
+  it("Initialize the vault!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accountsStrict({
+      user: signer.publicKey,
+      vaultState:vaultState,
+      vault:vault,
+      systemProgram:anchor.web3.SystemProgram.programId,
+    }).signers([signer]).rpc();
+
+    console.log("Transaction signature", tx);
   });
+
+
+  it("Deposit to the vault!", async () => {
+    const amount = 1*anchor.web3.LAMPORTS_PER_SOL;
+    const tx = await program.methods.deposit(new anchor.BN(amount)).accountsStrict({
+      user: signer.publicKey,
+      vaultState: vaultState,
+      vault: vault,
+      systemProgram:anchor.web3.SystemProgram.programId,
+    }).signers([signer]).rpc();
+
+    console.log("Transaction signature", tx);
+  })
 });
